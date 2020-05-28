3e74b2d6027f315011234faf43d3ce110a622f93
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-05-27 23:03:05 -0500

    periodic commit

--- main.js
+++ main.js
@@ -1,14 +1,72 @@
-var console_logging_active = false;
-
-var ableToScroll = false;
-
-function UpdatePrompter() {
+function updatePrompterDisplay() {
     prompter.innerText = inputField.value.toUpperCase();
 }
 
+
+function scrollDiv_init() {
+    // TODO: can we use the other selector instead of the id?
+    // EDIT: I tried it but the selector needs be the parent div not the prompter directly.
+    scrollingDiv = document.getElementById('box-1');
+    reachedMaxScroll = false;
+    scrollingDiv.scrollTop = 0;
+    previousScrollTop = 0;
+
+    ScrollInterval = setInterval('scrollDiv()', scrollSpeed);
+}
+
+function scrollDiv() {
+    if (ableToScroll) {
+        if (!reachedMaxScroll) {
+            scrollingDiv.scrollTop = previousScrollTop;
+            previousScrollTop++;
+            reachedMaxScroll = scrollingDiv.scrollTop >= (scrollingDiv.scrollHeight - scrollingDiv.offsetHeight);
+            console.log(("func " + scrollSpeed));
+        } else {
+            reachedMaxScroll = (scrollingDiv.scrollTop == 0) ? false : true;
+            scrollingDiv.scrollTop = previousScrollTop;
+            previousScrollTop--;
+        }
+    }
+}
+
+// this isnt used currently
+function pauseDiv() {
+    clearInterval(ScrollInterval);
+}
+
+// this isnt used currently
+function resumeDiv() {
+    previousScrollTop = scrollingDiv.scrollTop;
+    ScrollInterval = setInterval('scrollDiv()', scrollSpeed);
+}
+
+function increaseScrollSpeed() {
+    switch (scrollSpeed) {
+        case 10:
+            scrollSpeed = 8;
+            break;
+        case 8:
+            scrollSpeed = 2;
+            break;
+        case 2:
+            scrollSpeed = 10;
+            break;
+    }
+    clearInterval(ScrollInterval);
+    ScrollInterval = setInterval('scrollDiv()', scrollSpeed);
+}
+
+function swapColorScheme() {
+    // getComputedStyle(document.documentElement).setProperty('--primary-background-color', 'green');
+    document.documentElement.style.setProperty('--primary-background-color', 'green');
+}
+
 // Variables
-var scrollSpeed = 5; // How fast we scroll, maybe have a second value for slower rewind?
+var scrollSpeed = 10; // How fast we scroll, maybe have a second value for slower rewind?
 var horizontal = true; // A false value will load the vertical view option when we build that.
+var console_logging_active = false;
+var ableToScroll = false;
+var reachedMaxScroll = false;
 
 // Fields
 const prompter = document.querySelector('[data-prompter-field]')
@@ -23,7 +81,7 @@ const viewButton = document.querySelector('[data-view-btn]')
 // Listeners
 document.onkeydown = function(e) {
     e = e || window.event;
-    if (e.keyCode == 32) {
+    if (e.keyCode == 18) {
         ableToScroll = true;
         if (console_logging_active) console.log("Space pressed");
     }
@@ -31,7 +89,7 @@ document.onkeydown = function(e) {
 
 document.onkeyup = function(e) {
     e = e || window.event;
-    if (e.keyCode == 32) {
+    if (e.keyCode == 18) {
         ableToScroll = false;
         if (console_logging_active) console.log("Space lifted");
     }
@@ -39,11 +97,12 @@ document.onkeyup = function(e) {
 
 inputField.onkeyup = function() {
     if (console_logging_active) console.log("keyup");
-    UpdatePrompter();
+    updatePrompterDisplay();
 }
 
 speedButton.addEventListener('click', e => {
     if (console_logging_active) console.log("SPEED button clicked");
+    increaseScrollSpeed();
 })
 
 contrastButton.addEventListener('click', e => {
@@ -58,55 +117,7 @@ viewButton.addEventListener('click', e => {
     if (console_logging_active) console.log("VIEW button clicked");
 })
 
-
-// copied and pasted somewhat works lets rework this later.
-ScrollRate = 20;
-reachedMaxScroll = false;
-
-function scrollDiv_init() {
-
-    // TODO: can we use the other selector instead of the id?
-    // EDIT: i tried it but the selector needs be the parent div not the prompter directly.
-    scrollingDiv = document.getElementById('box-1');
-
-
-    reachedMaxScroll = false;
-
-    scrollingDiv.scrollTop = 0;
-    previousScrollTop = 0;
-
-    ScrollInterval = setInterval('scrollDiv()', ScrollRate);
-}
-
-function scrollDiv() {
-
-    if (ableToScroll) {
-        if (!reachedMaxScroll) {
-
-            scrollingDiv.scrollTop = previousScrollTop;
-            previousScrollTop++;
-
-            reachedMaxScroll = scrollingDiv.scrollTop >= (scrollingDiv.scrollHeight - scrollingDiv.offsetHeight);
-
-        } else {
-
-            reachedMaxScroll = (scrollingDiv.scrollTop == 0) ? false : true;
-
-            scrollingDiv.scrollTop = previousScrollTop;
-            previousScrollTop--;
-        }
-    }
-
-
-}
-
-function pauseDiv() {
-    clearInterval(ScrollInterval);
-}
-
-function resumeDiv() {
-    previousScrollTop = scrollingDiv.scrollTop;
-    ScrollInterval = setInterval('scrollDiv()', ScrollRate);
-}
-
+// inititializes the scrolling action
 scrollDiv_init();
+
+swapColorScheme();
